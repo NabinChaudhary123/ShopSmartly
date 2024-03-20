6b3d8b2b6acba552dee91d79eb84546d20f107d9
GoeIshikawa <nabin.chaudhary2290@gmail.com>
2024-03-20 21:11:51 +0545

    Initial Commit

---
+++ ShopSmartly/src/main/java/com/example/ShopSmartly/ShopSmartlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopSmartly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSmartlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSmartlyApplication.class, args);
+	}
+
+}
---
+++ ShopSmartly/src/test/java/com/example/ShopSmartly/ShopSmartlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopSmartly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
